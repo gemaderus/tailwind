ca23d1df0e1db0a9a0316fd2fb56f5d3a150b2b0
gemaderus <gderus@paradigmadigital.com>
2020-11-08 08:41:27 +0100

    Added setup react tailwind.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/Header';
+
+import './styles/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Header />
+    <svg className="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+  </svg>
+  </div>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import '../styles/main.css';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            Soy el header
+          </li>
+        </ul>
+        <button type="button" className="text">
+          <svg className="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
