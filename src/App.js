475f1f3fa9137986c95476f76ecd7d699a6cda85
gemaderus <gderus@paradigmadigital.com>
2020-11-15 13:11:33 +0100

    Added comoponents

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
+import Cards from './components/cards/Cards';
+import Footer from './components/footer/Footer';
 
-import './styles/main.css';
+import './styles/tailwind.output.css';
 
 function App() {
   return (
-    <div>
+    <div className="flex-col m-auto">
     <Header />
-    <svg className="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-  </svg>
+    <Hero />
+    <Cards />
+    <Footer />
   </div>
   );
 }
